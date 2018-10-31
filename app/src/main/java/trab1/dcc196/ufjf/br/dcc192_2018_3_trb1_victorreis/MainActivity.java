90e424bbb97427605e52da39a553386131065347
victorreis <v.crisostomo.reis@gmail.com>
2018-10-31 15:09:42 -0200

    Criada a tela inicial

--- app/src/main/java/trab1/dcc196/ufjf/br/dcc192_2018_3_trb1_victorreis/MainActivity.java
+++ app/src/main/java/trab1/dcc196/ufjf/br/dcc192_2018_3_trb1_victorreis/MainActivity.java
@@ -2,12 +2,49 @@ package trab1.dcc196.ufjf.br.dcc192_2018_3_trb1_victorreis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rv;
+    private Button btnCadastrarParticipante;
+    private Button btnCadastrarEvento;
+    private Button btnListarEventos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCadastrarParticipante = (Button) findViewById(R.id.btn_cadastrar_participante);
+        btnCadastrarParticipante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        btnCadastrarEvento = (Button) findViewById(R.id.btn_cadastrar_evento);
+        btnCadastrarEvento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnListarEventos = (Button) findViewById(R.id.btn_listar_eventos);
+        btnListarEventos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        rv = (RecyclerView) findViewById(R.id.main_recycle_view);
+        rv.setLayoutManager(new LinearLayoutManager(this));
     }
 }
