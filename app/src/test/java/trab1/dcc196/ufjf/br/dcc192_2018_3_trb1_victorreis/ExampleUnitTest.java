6d61ad2ffa7afde9586b40bc6ad8d92c09913e9c
victorreis <v.crisostomo.reis@gmail.com>
2018-10-31 13:15:52 -0200

    Commit Inicial

---
+++ app/src/test/java/trab1/dcc196/ufjf/br/dcc192_2018_3_trb1_victorreis/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package trab1.dcc196.ufjf.br.dcc192_2018_3_trb1_victorreis;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
