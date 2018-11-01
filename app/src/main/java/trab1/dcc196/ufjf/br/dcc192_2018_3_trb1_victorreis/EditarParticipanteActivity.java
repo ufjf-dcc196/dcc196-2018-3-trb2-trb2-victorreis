7571ac4b7089cf596b6553b09f28e88d66e53cbd
victorreis <v.crisostomo.reis@gmail.com>
2018-10-31 22:58:57 -0200

    Editando participantes (testes). Pequenas modificações em outros arquivos.

---
+++ app/src/main/java/trab1/dcc196/ufjf/br/dcc192_2018_3_trb1_victorreis/EditarParticipanteActivity.java
@@ -0,0 +1,65 @@
+package trab1.dcc196.ufjf.br.dcc192_2018_3_trb1_victorreis;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class EditarParticipanteActivity extends AppCompatActivity {
+
+    private EditText edtNomeCompleto;
+    private EditText edtEmail;
+    private EditText edtCPF;
+
+    private Button btnEditarParticipante;
+
+    private int participanteIndice;
+    private Participante participante;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_participante);
+
+        final Intent intent = getIntent();
+        Bundle bundleResult = intent.getExtras();
+        participanteIndice = bundleResult.getInt(MainActivity.PARTICIPANTE_INDICE);
+        participante = Persistencia.getInstanceParticipantes().get(participanteIndice);
+
+        edtNomeCompleto = (EditText) findViewById(R.id.edt_nome_completo);
+        edtNomeCompleto.setText(participante.getNomeCompleto());
+
+        edtEmail = (EditText) findViewById(R.id.edt_email);
+        edtEmail.setText(participante.getEmail());
+
+        edtCPF = (EditText) findViewById(R.id.edt_cpf);
+        edtCPF.setText(participante.getCpf());
+
+        btnEditarParticipante = (Button) findViewById(R.id.btn_editar_participante);
+        btnEditarParticipante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtNomeCompleto.getText() == null || "".equals(edtNomeCompleto.getText().toString()) ||
+                        edtEmail.getText() == null || "".equals(edtEmail.getText().toString()) ||
+                        edtCPF.getText() == null || "".equals(edtCPF.getText().toString())
+                        ) {
+                    Toast t = Toast.makeText(getApplicationContext(), "ERRO: Preencher todos os campos.", Toast.LENGTH_LONG);
+                    t.show();
+                    return;
+                }
+
+                Intent intentEditarParticipante = new Intent();
+                intentEditarParticipante.putExtra(ExibirDadosParticipanteActivity.PARTICIPANTE_NOME_COMPLETO, edtNomeCompleto.getText().toString());
+                intentEditarParticipante.putExtra(ExibirDadosParticipanteActivity.PARTICIPANTE_EMAIL, edtEmail.getText().toString());
+                intentEditarParticipante.putExtra(ExibirDadosParticipanteActivity.PARTICIPANTE_CPF, edtCPF.getText().toString());
+                setResult(Activity.RESULT_OK, intentEditarParticipante);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/trab1/dcc196/ufjf/br/dcc192_2018_3_trb1_victorreis/ExibirDadosParticipanteActivity.java
+++ app/src/main/java/trab1/dcc196/ufjf/br/dcc192_2018_3_trb1_victorreis/ExibirDadosParticipanteActivity.java
@@ -1,6 +1,7 @@
 package trab1.dcc196.ufjf.br.dcc192_2018_3_trb1_victorreis;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -21,6 +22,11 @@ public class ExibirDadosParticipanteActivity extends AppCompatActivity {
     private RecyclerView rvEventosRestantes;
 
     private static final int REQUEST_EDITAR_PARTICIPANTE = 1;
+
+    public static final String PARTICIPANTE_NOME_COMPLETO = "PARTICIPANTE_NOME_COMPLETO";
+    public static final String PARTICIPANTE_EMAIL = "PARTICIPANTE_EMAIL";
+    public static final String PARTICIPANTE_CPF = "PARTICIPANTE_CPF";
+
     private int participanteIndice;
     private Participante participante;
 
@@ -47,7 +53,9 @@ public class ExibirDadosParticipanteActivity extends AppCompatActivity {
         btnEditarParticipante.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(ExibirDadosParticipanteActivity.this, EditarParticipanteActivity.class);
+                intent.putExtra(MainActivity.PARTICIPANTE_INDICE, participanteIndice);
+                startActivityForResult(intent, REQUEST_EDITAR_PARTICIPANTE);
             }
         });
 
@@ -57,5 +65,22 @@ public class ExibirDadosParticipanteActivity extends AppCompatActivity {
         rvEventosRestantes = (RecyclerView) findViewById(R.id.rv_eventos_restantes);
         rvEventosRestantes.setLayoutManager(new LinearLayoutManager(this));
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ExibirDadosParticipanteActivity.REQUEST_EDITAR_PARTICIPANTE) {
+            Bundle bndResultado = data.getExtras();
+
+            String nomeCompleto = bndResultado.getString(ExibirDadosParticipanteActivity.PARTICIPANTE_NOME_COMPLETO);
+            String email = bndResultado.getString(ExibirDadosParticipanteActivity.PARTICIPANTE_EMAIL);
+            String cpf = bndResultado.getString(ExibirDadosParticipanteActivity.PARTICIPANTE_CPF);
+
+            participante.setNomeCompleto(nomeCompleto);
+            participante.setEmail(email);
+            participante.setCpf(cpf);
+        }
     }
 }
